c7e2aca10bc0840ceecc42b0bc95a7ebb234ef81
Daniel Gibson <danielgibson@Daniels-MacBook-Air.local>
2018-12-11 11:32:15 +1300

    All code added and tested

--- js/script.js
+++ js/script.js
@@ -2,49 +2,76 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+//Global Variables that store the DOM elements that will be referenced and/or manipulated
+const studentsPerPage = 10;
+const studentList = document.querySelector('.student-list').children;
+const pageDiv = document.querySelector('.page');
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//function to hide or show students according to the page number
+const showPage = (list, page) => {
+	let lastIndex = (studentsPerPage * page) - 1;
+	let firstIndex = lastIndex - (studentsPerPage - 1);
+
+  	//change the styles to hide or show accordingly
+  	for (let i = 0; i < list.length; i += 1) {
+    	if (i >= firstIndex && i <= lastIndex) {
+      	list[i].style.display = '';
+    } else {
+    	list[i].style.display = 'none';
+    }
+  }
+}
 
 
+//function to generate, append, and add functionality to the pagination buttons.
 
+const appendPageLinks = (list) => {
+	//1. Number of pages needed
+	const totalPages = Math.ceil(list.length / studentsPerPage);
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+	//2. Created 'div' with pagination class and append to '.page' div
+	const paginationDiv = document.createElement('div');
+	paginationDiv.classList.add('pagination');
+	pageDiv.appendChild(paginationDiv);
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+	//3. Add 'ul' to the pagination div to store the pagination links
+	const ul = document.createElement('ul');
+	paginationDiv.appendChild(ul);
 
+	//4. for every page, add 'li' and 'a' tags with the page number text
+	const li = document.createElement('li');
+	const a = document.createElement('a');
 
+	for (let i = 1; i <= totalPages; i += 1) {
+		ul.appendChild(li);
+		let a = document.createElement('a');
+		a.href = '#';
+		a.innerText = i;
+		li.appendChild(a);
+		if (i === 1) {
+			a.className = 'active';
+		}
+	
+		//5. Add event listener to each 'a' tag. When clicked call the showPage function to display the appropriate page
+		a.addEventListener('click', (event) => {
+			let currentPageNumber = event.target.textContent;
+			showPage(studentList, currentPageNumber);
+			const links = document.querySelectorAll('a');
 
+			//6. Loop over pagination links to remove active class from all links
+			const paginationLinks = document.querySelectorAll('.pagination ul li a');
+			for (let i = 0; i < paginationLinks.length; i += 1) {
+				paginationLinks[i].classList.remove('active');
+			}
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+			//7. Add the active class to the link that was just clicked.
+			event.target.className = 'active';
+		});
+	}
+}
 
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//execute the functions
+showPage(studentList, 1);
+appendPageLinks(studentList);
